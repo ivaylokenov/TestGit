939892268e5598fb1601f9f7b2992fddbbd7b60f
ivaylokenov <ivaylo.kenov@gmail.com>
2021-11-22 19:44:03 +0200

    Added my first homework - async await

---
+++ AsyncAwait/10. TasksBehindTheScenes/EventBasedApi.cs
@@ -0,0 +1,28 @@
+﻿namespace TasksBehindTheScenes
+{
+    using System;
+
+    public class EventBasedApi
+    {
+        public event Action<string> OnDone = s => { };
+
+        public void Work()
+        {
+            Console.WriteLine("Working...");
+
+            var end = DateTime.Now.AddSeconds(5);
+
+            while (true)
+            {
+                if (DateTime.Now > end)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Done...");
+
+            this.OnDone("Data");
+        }
+    }
+}
