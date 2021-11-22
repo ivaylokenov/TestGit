89244ca71d195e62a4b0605f8626d98e1fa086de
ivaylokenov <ivaylo.kenov@gmail.com>
2021-11-22 19:39:38 +0200

    Added source code

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyConsoleApp
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
